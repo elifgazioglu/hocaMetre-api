541822e7ad496ccc14f49f102f79f0c48edc5075
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-10-21 14:14:50 +0300

    add user and auth routes

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.route.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.listen(process.env.PORT || 8800, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("backend server is running");
 });
+
+app.get("/api/test", () => {
+  console.log("test is successfull");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
